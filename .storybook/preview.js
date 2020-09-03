60b94a8fab60d831d0168b5a227d70bc766562bc
vysakh-menon-aot <vysakh.menon@aot-technologies.com>
2020-09-03 15:10:37 -0700

    removed unwanted packages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,13 +5,9 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import Vuelidate from 'vuelidate'
-import Affix from 'vue-affix'
-import Vue2Filters from 'vue2-filters' // needed by SbcFeeSummary
 
 Vue.use(Vuetify)
 Vue.use(Vuelidate)
-Vue.use(Affix)
-Vue.use(Vue2Filters)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
