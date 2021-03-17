6c78ed366b5c5d923f356e37aecdce5adc9d1230
CameronFreshworks <53542131+cameron-freshworks@users.noreply.github.com>
2021-03-17 13:51:35 -0700

    Court Order and POA (#34)
    * Court Order and POA
    
    * Recovery and PR updates
    
    * Storybook build

---
+++ src/components/CourtOrderPoa/CourtOrderPoa.stories.ts
@@ -0,0 +1,19 @@
+import { CourtOrderPoa } from './index'
+
+export default {
+  title: 'component/CourtOrderPoa',
+  component: CourtOrderPoa,
+  argTypes: {
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { CourtOrderPoa },
+  template: '<court-order-poa v-bind="$props" />' // $props comes from args below
+})
+
+export const courtOrderPoa = Template.bind({})
+courtOrderPoa.args = {
+  validate: false
+}
