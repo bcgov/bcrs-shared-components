ff8f283503653c3b37dc7990549f6a0eaaeb2514
Severin Beauvais <severin.beauvais@gmail.com>
2021-10-27 15:59:25 -0700

    - reverted PM to pm in unit tests (Mac vs WSL2 different) - refactored unit test so wrapper is always destroyed and sinon is always restored (to prevent cascading errors)

--- tests/unit/StaffComments.spec.ts
+++ tests/unit/StaffComments.spec.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import axios from 'axios'
 import sinon from 'sinon'
-import { mount } from '@vue/test-utils'
+import { mount, Wrapper } from '@vue/test-utils'
 import flushPromises from 'flush-promises'
 import { sleep } from '@/utils/sleep'
 import { StaffComments } from '@/components/StaffComments'
@@ -29,13 +29,20 @@ const sampleComments = [
 ]
 
 describe('Staff Comments', () => {
+  let wrapper: Wrapper<StaffComments>
+
+  afterEach(() => {
+    wrapper.destroy()
+    sinon.restore()
+  })
+
   it('displays correctly with no previous comments', async () => {
     // mock GET comments endpoint
     sinon.stub(axios, 'get').withArgs('businesses/CP0000001/comments')
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000001', maxLength: 2000 }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000001', maxLength: 2000 }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -51,9 +58,6 @@ describe('Staff Comments', () => {
 
     // verify empty section
     expect(wrapper.findAll('#existing-comments > div').length).toBe(0)
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('displays correctly with existing comments', async () => {
@@ -63,7 +67,7 @@ describe('Staff Comments', () => {
       )
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000002' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000002' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -82,9 +86,6 @@ describe('Staff Comments', () => {
     expect(wrapper.find('#existing-comments > div').text()).toContain('A comment')
     expect(wrapper.find('#existing-comments > div').text()).toContain('Tester')
     expect(wrapper.find('#existing-comments > div').text()).toContain('Feb 4, 2021 at 2:08 pm Pacific time')
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('saves and updates correctly', async () => {
@@ -102,7 +103,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({})))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000003' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000003' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -128,9 +129,6 @@ describe('Staff Comments', () => {
 
     // verify updated comments list
     expect(vm.comments.length).toBe(1)
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('closes the dialog correctly', async () => {
@@ -139,7 +137,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000004' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000004' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -167,9 +165,6 @@ describe('Staff Comments', () => {
     // NB: the menu is still rendered
     expect(wrapper.find('#comments-button').attributes('aria-expanded')).toBe('false')
     expect(wrapper.find('.v-menu__content').isVisible()).toBe(false)
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('cancels the dialog correctly', async () => {
@@ -178,7 +173,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000005' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000005' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -206,9 +201,6 @@ describe('Staff Comments', () => {
     // NB: the menu is still rendered
     expect(wrapper.find('#comments-button').attributes('aria-expanded')).toBe('false')
     expect(wrapper.find('.v-menu__content').isVisible()).toBe(false)
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('validates empty comment correctly', async () => {
@@ -217,7 +209,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000006' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000006' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -241,9 +233,6 @@ describe('Staff Comments', () => {
     expect(wrapper.find('.v-textarea').classes('error--text')).toBe(true)
     expect(wrapper.findAll('.v-messages__message').length).toBe(1)
     expect(wrapper.find('.v-messages__message').text()).toContain('Enter a comment.')
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   // FUTURE: fix this test
@@ -253,7 +242,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000007', maxLength: 3 }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000007', maxLength: 3 }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -277,9 +266,6 @@ describe('Staff Comments', () => {
     expect(wrapper.find('.v-textarea').classes('error--text')).toBe(true)
     expect(wrapper.findAll('.v-messages__message').length).toBe(1)
     expect(wrapper.find('.v-messages__message').text()).toContain('Maximum characters reached.')
-
-    wrapper.destroy()
-    sinon.restore()
   })
 
   it('resets validation when reopened', async () => {
@@ -288,7 +274,7 @@ describe('Staff Comments', () => {
       .returns(new Promise(resolve => resolve({ data: { comments: [] } })))
 
     // mount the component
-    const wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000008' }, vuetify })
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'CP0000008' }, vuetify })
     const vm: any = wrapper.vm
     await flushPromises()
 
@@ -322,8 +308,76 @@ describe('Staff Comments', () => {
     // verify there are no error messages
     expect(wrapper.find('.v-textarea').classes('error--text')).toBe(false)
     expect(wrapper.findAll('.v-messages__message').length).toBe(0)
+  })
 
-    wrapper.destroy()
-    sinon.restore()
+  it('displays correctly with existing comments when passing external URL', async () => {
+    // mock GET comments endpoint
+    sinon.stub(axios, 'get').withArgs('http://test.com')
+      .returns(new Promise(resolve => resolve({ data: { comments: sampleComments } }))
+      )
+
+    // mount the component
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'RS001', url: 'http://test.com' }, vuetify })
+    const vm: any = wrapper.vm
+    await flushPromises()
+
+    // verify a few things
+    expect(vm.maxLength).toBe(4096)
+    expect(vm.comments.length).toBe(1)
+    expect(wrapper.find('#comments-button span').text()).toBe('1 Comment')
+
+    // open the dialog
+    // wait for Vue to finish transition
+    await wrapper.find('#comments-button').trigger('click')
+    await sleep(500)
+
+    // verify comment in div
+    expect(wrapper.findAll('#existing-comments > div').length).toBe(1)
+    expect(wrapper.find('#existing-comments > div').text()).toContain(sampleComments[0].comment.comment)
+    expect(wrapper.find('#existing-comments > div').text()).toContain(sampleComments[0].comment.submitterDisplayName)
+    expect(wrapper.find('#existing-comments > div').text()).toContain('Feb 4, 2021 at 2:08 pm Pacific time')
+  })
+
+  it('saves and updates correctly when URL passing', async () => {
+    // mock GET comments endpoint
+    const callback = sinon.stub(axios, 'get').withArgs('http://test.com')
+    callback.onCall(0).returns(new Promise(resolve => resolve({
+      data: { comments: [] }
+    })))
+    callback.onCall(1).returns(new Promise(resolve => resolve({
+      data: { comments: sampleComments }
+    })))
+
+    // mock POST comments endpoint
+    sinon.stub(axios, 'post').withArgs('http://test.com')
+      .returns(new Promise(resolve => resolve({})))
+
+    // mount the component
+    wrapper = mount(StaffComments, { propsData: { axios, businessId: 'RS001', url: 'http://test.com' }, vuetify })
+    const vm: any = wrapper.vm
+    await flushPromises()
+
+    // verify initial comments list
+    expect(vm.comments.length).toBe(0)
+
+    // open the dialog
+    // wait for Vue to finish transition
+    await wrapper.find('#comments-button').trigger('click')
+    await sleep(500)
+
+    // verify that textarea is empty
+    const textarea = wrapper.find('textarea')
+    expect((textarea.element as HTMLInputElement).value).toBe('')
+
+    // enter a comment and verify textarea again
+    await textarea.setValue('A comment')
+    expect((textarea.element as HTMLInputElement).value).toBe('A comment')
+
+    // save the comment
+    await wrapper.find('#save-button').trigger('click')
+    await flushPromises()
+
+    // verify updated comments list
+    expect(vm.comments.length).toBe(1)
   })
 })
