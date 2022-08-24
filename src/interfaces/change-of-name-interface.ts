baf1a802aa09f450f4f9d17e9ecd488c52453f72
Travis Semple <travis8814@gmail.com>
2022-08-24 08:20:58 -0700

    Use ChangeOfNameIF.

--- src/interfaces/change-of-name-interface.ts
+++ src/interfaces/change-of-name-interface.ts
@@ -17,3 +17,5 @@ export interface ChangeOfNameNameRequestIF {
 export interface ChangeOfNameLegalNameIF {
     legalName: string
 }
+
+export type ChangeOfNameIF = ChangeOfNameLegalNameIF | ChangeOfNameNameRequestIF
