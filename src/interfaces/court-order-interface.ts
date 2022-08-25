0eaaeaabbe44283553ba50deb85f9a9d1bbda026
Travis Semple <travis8814@gmail.com>
2022-08-25 08:44:35 -0700

    Minor formatting fix.

--- src/interfaces/court-order-interface.ts
+++ src/interfaces/court-order-interface.ts
@@ -2,7 +2,7 @@
  * See:
  * https://github.com/bcgov/business-schemas/blob/master/src/registry_schemas/schemas/court_order.json
  */
-export interface CourtOrderIF{
+export interface CourtOrderIF {
   fileNumber?: string
   orderDate?: string
   effectOfOrder?: string,
