2694689e0b8c7c342258ca700b2388270492a519
Travis Semple <travis8814@gmail.com>
2022-08-26 11:52:56 -0700

    Putting optional fields back.

--- src/interfaces/court-order-interface.ts
+++ src/interfaces/court-order-interface.ts
@@ -3,8 +3,8 @@
  * https://github.com/bcgov/business-schemas/blob/master/src/registry_schemas/schemas/court_order.json
  */
 export interface CourtOrderIF {
-  fileNumber?: string
-  orderDate?: string
-  effectOfOrder?: string,
+  fileNumber: string
+  orderDate: string
+  effectOfOrder: string,
   hasPlanOfArrangement?: boolean
 }
