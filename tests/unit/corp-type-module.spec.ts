6f762c3e0870a4bc8c3e6fb3a866fda3f6770748
Severin Beauvais <severin.beauvais@gmail.com>
2022-11-02 13:41:11 -0700

    - added missing BC_CCC numbered description - fixed unit test name - updated unit tests

--- tests/unit/corp-type-module.spec.ts
+++ tests/unit/corp-type-module.spec.ts
@@ -6,7 +6,7 @@ import {
 
 const Dummy = Vue.component('dummy-component', { template: '<div />' })
 
-describe('Corp Type Util', () => {
+describe('Corp Type Module', () => {
   let vm: any
 
   beforeAll(async () => {
@@ -28,18 +28,20 @@ describe('Corp Type Util', () => {
     expect(obj.numberedDesc).toBe('Numbered Company')
   })
 
-  it('gets corp full description correctly', () => {
+  it('gets corp full descriptions correctly', () => {
     expect(GetCorpFullDescription(CorpTypeCd.BC_COMPANY)).toBe('BC Limited Company')
     expect(GetCorpFullDescription(CorpTypeCd.BC_ULC_COMPANY)).toBe('BC Unlimited Liability Company')
     expect(GetCorpFullDescription(CorpTypeCd.BENEFIT_COMPANY)).toBe('BC Benefit Company')
+    expect(GetCorpFullDescription(CorpTypeCd.BC_CCC)).toBe('BC Community Contribution Company')
     expect(GetCorpFullDescription(CorpTypeCd.COOP)).toBe('BC Cooperative Association')
     expect(GetCorpFullDescription(null)).toBe('')
   })
 
-  it('gets corp numbered description correctly', () => {
+  it('gets corp numbered descriptions correctly', () => {
     expect(GetCorpNumberedDescription(CorpTypeCd.BC_COMPANY)).toBe('Numbered Limited Company')
     expect(GetCorpNumberedDescription(CorpTypeCd.BC_ULC_COMPANY)).toBe('Numbered Unlimited Liability Company')
     expect(GetCorpNumberedDescription(CorpTypeCd.BENEFIT_COMPANY)).toBe('Numbered Benefit Company')
+    expect(GetCorpNumberedDescription(CorpTypeCd.BC_CCC)).toBe('Numbered Community Contribution Company')
     expect(GetCorpNumberedDescription(CorpTypeCd.COOP)).toBe('Numbered Cooperative Association')
     expect(GetCorpNumberedDescription(null)).toBe('')
   })
