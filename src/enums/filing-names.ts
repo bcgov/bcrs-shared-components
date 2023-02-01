54264036d4f4aa64eda4b0e847ee1823173c9eef
Severin Beauvais <severin.beauvais@gmail.com>
2023-02-01 12:37:05 -0800

    - updated filing codes enum - added filing names enum - added filing types enum - added restoration types enum

---
+++ src/enums/filing-names.ts
@@ -0,0 +1,12 @@
+export enum FilingNames {
+  ALTERATION = 'Alteration',
+  CHANGE_OF_REGISTRATION = 'Change of Registration',
+  CONVERSION = 'Conversion',
+  CORRECTION = 'Correction',
+  DISSOLUTION_FIRM = 'Dissolution',
+  INCORPORATION_APPLICATION = 'Incorporation Application',
+  REGISTRATION = 'Registration',
+  RESTORATION_APPLICATION = 'Restoration Application',
+  SPECIAL_RESOLUTION = 'Special Resolution',
+  VOLUNTARY_DISSOLUTION = 'Voluntary Dissolution',
+}
---
+++ src/enums/filing-types.ts
@@ -0,0 +1,12 @@
+export enum FilingTypes {
+  ALTERATION = 'alteration',
+  CHANGE_OF_REGISTRATION = 'changeOfRegistration',
+  CONVERSION = 'conversion',
+  CORRECTION = 'correction',
+  DISSOLUTION = 'dissolution',
+  INCORPORATION_APPLICATION = 'incorporationApplication',
+  REGISTRATION = 'registration',
+  RESTORATION = 'restoration',
+  SPECIAL_RESOLUTION = 'specialResolution',
+  VOLUNTARY_DISSOLUTION = 'dissolution',
+}
