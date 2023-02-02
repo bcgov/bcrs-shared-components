e50db82844c59b2577966de95cba7d0374c850c9
Karim El Jazzar <karim.jazzar@gov.bc.ca>
2023-02-02 11:42:59 -0800

    Updated to respond to Sev's PR comments

--- tests/unit/RelationshipsPanel.spec.ts
+++ tests/unit/RelationshipsPanel.spec.ts
@@ -4,6 +4,7 @@ import { createLocalVue, mount, Wrapper } from '@vue/test-utils'
 import { RelationshipsPanel } from '@/components/relationships-panel'
 import { ApprovalType } from '@/components/approval-type'
 import VueRouter from 'vue-router'
+import { RelationshipTypes } from '@/enums/relationship-types'
 
 Vue.use(Vuetify)
 let vuetify = new Vuetify({})
@@ -15,11 +16,11 @@ localVue.use(VueRouter)
  * Creates and mounts a blank, un-populated component
  */
 function createDefaultComponent (
-    draftRelationships: string[] = ['Heir or Legal Representative']
+  draftRelationships: RelationshipTypes[] = []
 ): Wrapper<RelationshipsPanel> {
   return mount(RelationshipsPanel, {
     propsData: {
-        draftRelationships,
+      draftRelationships
     },
     vuetify,
     localVue
@@ -34,13 +35,13 @@ describe('Initialize RelationshipsPanel component', () => {
   })
 
   it('no relationship selected (draft relationship empty)', async () => {
-    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent([])
+    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent()
     expect(wrapper.vm.$data.selectedRelationships).toEqual([])
     wrapper.destroy()
   })
 
   it('component emits events when no relationship is selected (validate)', async () => {
-    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent([])
+    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent()
     await Vue.nextTick()
 
     expect(wrapper.emitted('emitRelationshipsValid').pop()[0]).toEqual(false)
@@ -48,22 +49,22 @@ describe('Initialize RelationshipsPanel component', () => {
   })
 
   it('component emits events when a relationship is selected (validate)', async () => {
-    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent()
+    const wrapper: Wrapper<RelationshipsPanel> = createDefaultComponent([RelationshipTypes.HEIR_LEGAL_REP])
     await Vue.nextTick()
 
     expect(wrapper.emitted('emitRelationshipsValid').pop()[0]).toEqual(true)
     wrapper.destroy()
   })
 
-  it('component emits events (array) when a relationships are selected', async () => {
-    const wrapper: Wrapper<ApprovalType> = createDefaultComponent()
-    // Shareholder selected
-    const input = wrapper.find('#shareholder-checkbox')
+  it('component emits events (array) when relationships are selected', async () => {
+    const wrapper: Wrapper<ApprovalType> = createDefaultComponent([RelationshipTypes.HEIR_LEGAL_REP])
+    // Officer selected
+    const input = wrapper.find('#officer-checkbox')
     input.setChecked()
     await Vue.nextTick()
 
-    expect(wrapper.emitted('emitRelationshipsChanged').pop()[0]).toEqual(['Heir or Legal Representative', 'Shareholder'])
+    expect(wrapper.emitted('emitRelationshipsChanged').pop()[0])
+      .toEqual([RelationshipTypes.HEIR_LEGAL_REP, RelationshipTypes.OFFICER])
     wrapper.destroy()
   })
-
 })
