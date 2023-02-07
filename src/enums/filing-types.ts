d3483795c09783e5ed024314a30256eb725fd254
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2023-02-07 10:00:51 -0800

    - added some filing codes (#162)
    - added some filing names
    - added some filing types

--- src/enums/filing-types.ts
+++ src/enums/filing-types.ts
@@ -1,12 +1,28 @@
 export enum FilingTypes {
+  ADMIN_FREEZE = 'adminFreeze',
   ALTERATION = 'alteration',
+  ANNUAL_REPORT = 'annualReport',
+  CHANGE_OF_ADDRESS = 'changeOfAddress',
+  CHANGE_OF_COMPANY_INFO = 'changeOfCompanyInfo', // a type of alteration
+  CHANGE_OF_DIRECTORS = 'changeOfDirectors',
+  CHANGE_OF_NAME = 'changeOfName',
   CHANGE_OF_REGISTRATION = 'changeOfRegistration',
+  CONSENT_CONTINUATION_OUT = 'consentContinuationOut',
   CONVERSION = 'conversion',
   CORRECTION = 'correction',
+  COURT_ORDER = 'courtOrder',
   DISSOLUTION = 'dissolution',
+  DISSOLVED = 'dissolved',
   INCORPORATION_APPLICATION = 'incorporationApplication',
+  INVOLUNTARY_DISSOLUTION = 'involuntaryDissolution',
+  PUT_BACK_ON = 'putBackOn',
   REGISTRATION = 'registration',
+  REGISTRARS_NOTATION = 'registrarsNotation',
+  REGISTRARS_ORDER = 'registrarsOrder',
   RESTORATION = 'restoration',
   SPECIAL_RESOLUTION = 'specialResolution',
-  VOLUNTARY_DISSOLUTION = 'dissolution',
+  TRANSITION = 'transition',
+  VOLUNTARY_DISSOLUTION = 'voluntaryDissolution',
+
+  UNKNOWN = 'unknown' // for fallback
 }
