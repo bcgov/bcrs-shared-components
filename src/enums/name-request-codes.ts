2d4d4b383a0f920d44f775d6a04c9771b1da9234
Severin Beauvais <severin.beauvais@gmail.com>
2023-02-27 09:17:56 -0800

    - added/updated some Name Request Code comments

--- src/enums/name-request-codes.ts
+++ src/enums/name-request-codes.ts
@@ -5,19 +5,19 @@
 
 // ref: https://github.com/bcgov/namex/blob/main/api/namex/constants/__init__.py
 export enum NrRequestTypeCodes {
-  // these will be removed - use new codes below instead
-  BC_COMPANY = 'BC',
-  BC_CORPORATION = 'CR', // new or AML or resubmit
-  BC_UNLIMITED = 'UL', // new or AML or resubmit
-  COOP = 'CP', // new or AML or resubmit
-  NR_SOLE_PROP = 'FR', // new or resubmit firm or SP
-  XPRO_CORPORATION = 'XCR', // new or resubmit
+  // DEPRECATION WARNING - these will be removed!
+  BC_COMPANY = 'BC', // use NEW_BC instead
+  BC_CORPORATION = 'CR', // use NEW_CORP instead
+  BC_UNLIMITED = 'UL', // use NEW_ULC instead
+  COOP = 'CP', // use NEW_COOP instead
+  NR_SOLE_PROP = 'FR', // use NEW_FIRM instead
+  XPRO_CORPORATION = 'XCR', // use NEW_XPRO_CORP instead
 
   // change name (or resubmit)
   CHANGE_BEN = 'BEC',
   CHANGE_CCC = 'CCC',
   CHANGE_COOP = 'CCP',
-  CHANGE_CORP = 'CCR',
+  CHANGE_CORP = 'CCR', // BC Limited
   CHANGE_FIRM = 'CFR', // SP, DBA or GP
   CHANGE_ULC = 'CUL',
 
@@ -38,7 +38,7 @@ export enum NrRequestTypeCodes {
   NEW_BC = 'BC',
   NEW_CCC = 'CC',
   NEW_COOP = 'CP',
-  NEW_CORP = 'CR',
+  NEW_CORP = 'CR', // BC Limited
   NEW_FIRM = 'FR', // SP, DBA or GP
   NEW_ULC = 'UL',
   NEW_XPRO_CORP = 'XCR',
@@ -46,7 +46,7 @@ export enum NrRequestTypeCodes {
   // restoration
   RESTORATION_CCC = 'RCC',
   RESTORATION_COOP = 'RCP',
-  RESTORATION_CORP = 'RCR',
+  RESTORATION_CORP = 'RCR', // BC Limited
   RESTORATION_SOC = 'RSO',
   RESTORATION_BEN = 'BERE',
   RESTORATION_ULC = 'RUL',
@@ -59,8 +59,8 @@ export enum NrRequestActionCodes {
   CONVERSION = 'CNV',
   DBA = 'DBA',
   MOVE = 'MVE',
-  NEW_BUSINESS = 'NEW',
-  RESTORE = 'REH',
+  NEW_BUSINESS = 'NEW', // incorporate or register
+  RESTORE = 'REH', // restore or reinstate
   RENEW = 'REN',
   RESTORATION = 'REST', // FUTURE: is this used?
   RESUBMIT = 'RESUBMIT', // FUTURE: is this used?
