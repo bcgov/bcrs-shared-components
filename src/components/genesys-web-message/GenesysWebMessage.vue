2ecef52d8fa3731e9e89d44e9f3c1c44f26997f0
JazzarKarim <122301442+JazzarKarim@users.noreply.github.com>
2023-02-27 11:09:16 -0800

    15183 - Added Genesys Web Message Component (#171)
    * 15183 - Added Genesys Web Message Component
    
    * Fixed indentation
    
    * Only run if key is not equal to 0
    
    * Fixed indentation
    
    * Updated to respond to Sev's PR comments
    
    * 2nd Update to respond to Sev's PR comments
    
    * 3rd Update to respond to Sev
    
    * 4th update to respond to PR comment
    
    * Updated to respond to Sev's comments
    
    * Hotfix lifecycle method
    
    * Added comment
    
    * Fixed typo
    
    * Update after rebase
    
    * 2nd update after new rebase

---
+++ src/components/genesys-web-message/GenesysWebMessage.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="d-none"></div>
+</template>
+
+<script lang="ts">
+// Libraries
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+/**
+ * @property {string} genesysURL - the URL to the genesys library
+ * @property {string} environmentKey - the environment key (ID)
+ * @property {string} deploymentKey - the deployment key (ID)
+ * Go To README.md for more details.
+ */
+@Component({})
+export default class GenesysWebMessage extends Vue {
+  @Prop({ default: null }) readonly genesysURL!: string
+
+  @Prop({ default: null }) readonly environmentKey!: string
+
+  @Prop({ default: '0' }) readonly deploymentKey!: string
+
+  mounted (): void {
+    if (this.genesysURL && this.environmentKey && this.deploymentKey !== '0') {
+      this.initWebMsg(
+        window,
+        'Genesys',
+        this.genesysURL,
+        {
+          environment: this.environmentKey,
+          deploymentId: this.deploymentKey
+        },
+        null
+      )
+    }
+  }
+
+  // Clear chat history (local variable) when the component is destroyed.
+  beforeDestroy (): void {
+    localStorage.removeItem('_actmu')
+  }
+
+  /**
+   * From MAXIMUS Canada.
+   * The New Web-Messaging Solution in Genesys.
+   * @param {Window} g the global object
+   * @param {string} e the 'Genesys' string
+   * @param {string} n the link to the Genesys third-party library
+   * @param {string} es the object with the environment and deployment IDs
+   * @param {HTMLScriptElement} ys the element to be created
+   */
+  private initWebMsg (g, e, n, es, ys): void {
+    g['_genesysJs'] = e
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    }
+    g[e].t = (new Date()).getTime()
+    g[e].c = es
+    ys = document.createElement('script')
+    ys.async = 1
+    ys.src = n
+    ys.charset = 'utf-8'
+    document.head.appendChild(ys)
+    localStorage.removeItem('_actmu')
+  }
+}
+</script>
---
+++ src/components/genesys-web-message/README.md
@@ -0,0 +1,25 @@
+# Genesys Web Message
+
+- This component creates the button to generate a chat with Service BC Helpdesk.
+
+- This component cannot be unit tested because it heavily relies on a third-party library.
+
+Props
+-
+- The genesys URL which is the URL provided by MAXIMUS Canada to their Third-Party Library.
+- The Environment Key which is provided by MAXIMUS Canada.
+- The Deployment Key which is also provided by MAXIMUS Canada depending on which UI the component is going to be on.
+
+How To Use
+-
+- Insert the component into the preferred UI with the correct properties. For example:
+```
+<GenesysWebMessage
+    v-if="showWebMessage"
+    genesysURL="https://example.cloud/example.min.js"
+    environmentKey="xyz1"
+    deploymentKey="aaaa-bbbb-cccc-dddd-eeee"
+/>
+```
+- A button will appear on the bottom-right of the page.
+- Press it to open a new chat with the BC Helpdesk.
