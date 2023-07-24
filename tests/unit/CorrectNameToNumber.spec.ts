71afa5a776f43833f51448c87a31f18021993f48
Kevin Zhang <kevinzhang940420@gmail.com>
2023-07-24 08:38:53 -0700

    update CorrectNameToNumber test

--- tests/unit/CorrectNameToNumber.spec.ts
+++ tests/unit/CorrectNameToNumber.spec.ts
@@ -1,13 +1,15 @@
-import Vue from 'vue'
 import Vuetify from 'vuetify'
 import flushPromises from 'flush-promises'
 import { mount, Wrapper } from '@vue/test-utils'
 import CorrectNameToNumber from '@/components/correct-name/CorrectNameToNumber.vue'
+import { CorpTypeCd, CorrectNameOptions } from '@bcrs-shared-components/enums'
+
+const vuetify = new Vuetify({})
 
 function getLastEvent (wrapper: Wrapper<CorrectNameToNumber>, name: string): any {
-  const eventsList = wrapper.emitted(name) as Array<any>
+  const eventsList: Array<any> = wrapper.emitted(name)
   if (eventsList) {
-    const events = eventsList[eventsList.length - 1] as Array<any>
+    const events: Array<any> = eventsList[eventsList.length - 1]
     return events[0]
   }
   return null
@@ -15,19 +17,15 @@ function getLastEvent (wrapper: Wrapper<CorrectNameToNumber>, name: string): any
 
 const defaultProps = {
   businessId: 'BC0871437',
-  entityType: 'BC',
-  formType: 'correct-name-to-number',
+  entityType: CorpTypeCd.BC_COMPANY,
+  formType: '',
   validate: false
 }
 
 describe('CorrectNameToNumber', () => {
-  let vuetify: any
   let wrapperFactory: any
 
   beforeEach(() => {
-    // *** TODO: assign this outside describe block?
-    vuetify = new Vuetify({})
-
     // *** TODO: set this as a prop
     // store.state.stateModel.nameRequest = {
     //   legalName: 'Bobs Plumbing',
@@ -53,47 +51,48 @@ describe('CorrectNameToNumber', () => {
 
   it('verifies the checkbox default state', async () => {
     const wrapper = wrapperFactory()
-    await Vue.nextTick()
-
     const nameToNumberInput = wrapper.find('#correct-name-to-number-checkbox')
+
+    await flushPromises()
 
     // Verify data
     expect(nameToNumberInput.attributes('aria-checked')).toBe('false')
-    expect(wrapper.emitted('valid')).toStrictEqual([[true, true, false], [false, false, true]])
-    expect(wrapper.emitted('update:nameRequest')).toBeUndefined()
+
+    // formValid set false initially
+    expect(getLastEvent(wrapper, 'valid')).toBe(false)
   })
 
   it('verifies the emission when checkbox state changes', async () => {
     const wrapper = wrapperFactory()
-    await Vue.nextTick()
-
     const nameToNumberInput = wrapper.find('#correct-name-to-number-checkbox')
+
+    await flushPromises()
 
     // Verify data
     expect(nameToNumberInput.attributes('aria-checked')).toBe('false')
-    expect(wrapper.emitted('valid')).toStrictEqual([[true, true, false], [false, false, true]])
 
     // Select Name to Number Checkbox
     await nameToNumberInput.trigger('click')
 
     // Verify local state change and event emission
     expect(nameToNumberInput.attributes('aria-checked')).toBe('true')
+
+    // Submit Change
+    await wrapper.setProps({ formType: CorrectNameOptions.CORRECT_NAME_TO_NUMBER })
+    await flushPromises()
+
     expect(getLastEvent(wrapper, 'valid')).toBe(true)
-    const nameRequest = wrapper.emitted('update:nameRequest')
-    expect(nameRequest).toBeUndefined()
-    // expect(nameRequest.legalType).toBe('BEN')
-    // expect(nameRequest.legalName).toBe('Bobs Plumbing')
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe('0871437 B.C. LTD.')
   })
 
   it('verifies the form submission and verify global state change', async () => {
     const wrapper = wrapperFactory()
     const nameToNumberInput = wrapper.find('#correct-name-to-number-checkbox')
 
-    await Vue.nextTick()
+    await flushPromises()
 
     // Verify data
     expect(nameToNumberInput.attributes('aria-checked')).toBe('false')
-    expect(wrapper.emitted('valid')).toStrictEqual([[true, true, false], [false, false, true]])
 
     // Select Name to Number Checkbox
     await nameToNumberInput.trigger('click')
@@ -101,18 +100,49 @@ describe('CorrectNameToNumber', () => {
     // Verify local state change and event emission
     expect(nameToNumberInput.attributes('aria-checked')).toBe('true')
     expect(getLastEvent(wrapper, 'valid')).toBe(true)
-    expect(wrapper.emitted('update:nameRequest')).toBeUndefined()
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe(null)
 
     // Submit Change
-    await wrapper.setProps({ formType: 'correct-name-to-number' })
+    await wrapper.setProps({ formType: CorrectNameOptions.CORRECT_NAME_TO_NUMBER })
     await flushPromises()
 
-    expect(getLastEvent(wrapper, 'saved')).toBe(null)
+    expect(getLastEvent(wrapper, 'saved')).toBe(true)
 
     // Verify Data changes
-    const nameRequest = wrapper.emitted('update:nameRequest')
-    // expect(nameRequest.legalType).toBe('BEN')
-    // expect(nameRequest.legalName).toBeUndefined()
-    // expect(nameRequest.nrNumber).toBeUndefined()
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe('0871437 B.C. LTD.')
+
+    // Submit Change
+    await wrapper.setProps({ formType: CorrectNameOptions.CORRECT_NAME_TO_NUMBER, entityType: CorpTypeCd.BC_ULC_COMPANY })
+    await flushPromises()
+
+    // Verify Data changes
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe('0871437 B.C. LTD.')
+  })
+
+  it('verifies the form submission, verify global state change, verify name rules', async () => {
+    const wrapper = wrapperFactory()
+    const nameToNumberInput = wrapper.find('#correct-name-to-number-checkbox')
+
+    await flushPromises()
+
+    // Verify data
+    expect(nameToNumberInput.attributes('aria-checked')).toBe('false')
+
+    // Select Name to Number Checkbox
+    await nameToNumberInput.trigger('click')
+
+    // Verify local state change and event emission
+    expect(nameToNumberInput.attributes('aria-checked')).toBe('true')
+    expect(getLastEvent(wrapper, 'valid')).toBe(true)
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe(null)
+
+    // Submit Change
+    await wrapper.setProps({ formType: CorrectNameOptions.CORRECT_NAME_TO_NUMBER, entityType: CorpTypeCd.BC_ULC_COMPANY })
+    await flushPromises()
+
+    expect(getLastEvent(wrapper, 'saved')).toBe(true)
+
+    // Verify Data changes
+    expect(getLastEvent(wrapper, 'update:companyName')).toBe('0871437 B.C. UNLIMITED LIABILITY COMPANY')
   })
 })
