bff2491fb292bfea5567284647827e0d0d2f8cdb
Severin Beauvais <severin.beauvais@gmail.com>
2023-07-24 11:51:17 -0700

    - added Name Request Type codes (may be legacy or future)

--- src/enums/name-request-codes.ts
+++ src/enums/name-request-codes.ts
@@ -4,6 +4,7 @@
 //
 
 // ref: https://github.com/bcgov/namex/blob/main/api/namex/constants/__init__.py
+// ref: https://github.com/bcgov/lear/blob/main/legal-api/src/legal_api/resources/v1/nr_type_map.py
 export enum NrRequestTypeCodes {
   // change name (or resubmit)
   CHANGE_BEN = 'BEC',
@@ -21,10 +22,12 @@ export enum NrRequestTypeCodes {
   CONTINUATION_IN_ULC = 'ULCT',
 
   // convert (or resubmit)
-  CONVERT_BEN = 'BECV',
-  CONVERT_CCC = 'CCV',
-  CONVERT_CORP = 'BECR',
-  CONVERT_ULC = 'UC',
+  CONVERT_BEN = 'BECV', // from LTD to BEN
+  CONVERT_CCC = 'CCV', // from LTD to CCC
+  CONVERT_CORP = 'BECR', // from BEN to LTD
+  CONVERT_ULBE = 'ULBE', // from ULC to BEN
+  CONVERT_ULC = 'UC', // from LTD to ULC
+  CONVERT_ULCB = 'ULCB', // from ULC to LTD
 
   // new (or resubmit)
   NEW_BC = 'BC', // BC Benefit Company Incorporation
@@ -41,12 +44,44 @@ export enum NrRequestTypeCodes {
   RESTORATION_CORP = 'RCR', // BC Limited
   RESTORATION_SOC = 'RSO',
   RESTORATION_BEN = 'BERE',
-  RESTORATION_ULC = 'RUL'
+  RESTORATION_ULC = 'RUL',
+
+  // others, may be legacy or future
+  LC = 'LC', // new XPRO_LL_PARTNR
+  CLC = 'CLC', // change XPRO_LL_PARTNR
+  RLC = 'RLC', // restoration XPRO_LL_PARTNR
+  AL = 'AL', // assumed name XPRO_LL_PARTNR
+  LL = 'LL', // new LL_PARTNERSHIP
+  CLL = 'CLL', // change LL_PARTNERSHIP
+  XLL = 'XLL', // new XPRO_LL_PARTNR
+  XCLL = 'XCLL', // change XPRO_LL_PARTNR
+  LP = 'LP', // new LIM_PARTNERSHIP
+  CLP = 'CLP', // change LIM_PARTNERSHIP
+  SO = 'SO', // new SOCIETY
+  ASO = 'ASO', // amlagamate SOCIETY
+  CSO = 'CSO', // change SOCIETY
+  RSO = 'RSO', // restoration SOCIETY
+  CTSO = 'CTSO', // move SOCIETY
+  CSSO = 'CSSO', // convert SOCIETY
+  XSO = 'XSO', // new XPRO_SOCIETY
+  XCSO = 'XCSO', // change XPRO_SOCIETY
+  XRSO = 'XRSO', // restoration XPRO_SOCIETY
+  XASO = 'XASO', // assumed name XPRO_SOCIETY
+  XCASO = 'XCASO', // achg XPRO_SOCIETY
+  XCP = 'XCP', // new XPRO_COOP
+  XCCP = 'XCCP', // change XPRO_COOP
+  XRCP = 'XRCP', // restoration XPRO_COOP
+  FIN = 'FIN', // new FINANCIAL
+  CFI = 'CFI', // change FINANCIAL
+  RFI = 'RFI', // restoration FINANCIAL
+  PA = 'PA', // new PRIVATE_ACT
+  PAR = 'PAR', // new PARISHES
+  BEAM = 'BEAM', // amalgamate BCOMP
 }
 
 export enum NrRequestActionCodes {
   AMALGAMATE = 'AML',
-  ASSUMED = 'ASSUMED', // FUTURE: should be AS?
+  ASSUMED = 'ASSUMED', // FUTURE: should be AS (as in LEAR)?
   CHANGE_NAME = 'CHG',
   CONVERSION = 'CNV',
   DBA = 'DBA', // doing business as
