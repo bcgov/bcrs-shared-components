7e23de6d2d8ab63ec3706ab743f3f4d93c6884dd
Kevin Zhang <kevinzhang940420@gmail.com>
2023-07-25 14:03:31 -0700

    mock the console.warn

--- tests/unit/CorrectNameRequest.spec.ts
+++ tests/unit/CorrectNameRequest.spec.ts
@@ -3,7 +3,8 @@ import flushPromises from 'flush-promises'
 import { mount, Wrapper } from '@vue/test-utils'
 import CorrectNameRequest from '@/components/correct-name/CorrectNameRequest.vue'
 
-// mock the window.alert function to hide "[Vuetify] Unable to locate target XXX"
+// mock the window.alert function, console.warn to hide "[Vuetify] Unable to locate target XXX"
+console.warn = vitest.fn()
 window.alert = vitest.fn()
 
 function getLastEvent (wrapper: Wrapper<any>, name: string): any {
