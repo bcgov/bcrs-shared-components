b2bc2522c5e8b8c5066fea7abcee094bde94029f
JazzarKarim <karim.jazzar@gov.bc.ca>
2023-08-09 08:36:24 -0700

    Build Storybook

---
+++ docs/assets/BaseAddress.stories-89427432.js
@@ -0,0 +1,24 @@
+import{B as m}from"./BaseAddress-d969009f.js";import{V as c}from"./vuetify-abd37748.js";import"./vue.esm-b1e57a2f.js";import"./_commonjsHelpers-042e6b4d.js";import"./_plugin-vue2_normalizer-b11a0120.js";import"./Watch-62f8b699.js";import"./lodash-bc5bedbb.js";import"./validation-mixin-aac6b4a2.js";const V={title:"component/BaseAddress"},p=(l,{argTypes:n})=>({vuetify:new c({iconfont:"mdi"}),props:Object.keys(n),components:{BaseAddress:m},template:'<BaseAddress v-bind="$props" />'}),e=p.bind({});e.args={editing:!0,schema:null,address:{}};const s=p.bind({});s.args={editing:!0,schema:null,address:{streetAddress:"1234 Sesame Street",streetAddressAdditional:"4th Floor",addressCity:"Victoria",addressRegion:"British Columbia",addressCountry:"Canada",postalCode:"V8N 1A1",deliveryInstructions:"Leave at front door"},noPoBox:!0};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    BaseAddress
+  },
+  template: '<BaseAddress v-bind="$props" />' // $props comes from args below
+})`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var a,d,i;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    BaseAddress
+  },
+  template: '<BaseAddress v-bind="$props" />' // $props comes from args below
+})`,...(i=(d=s.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const w=["DefaultBaseAddress","FilledInBaseAddress"];export{e as DefaultBaseAddress,s as FilledInBaseAddress,w as __namedExportsOrder,V as default};
+//# sourceMappingURL=BaseAddress.stories-89427432.js.map
