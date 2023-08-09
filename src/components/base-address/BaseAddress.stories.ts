246e9d8e1d5aec84e476e04ac66cbfbaac09966a
JazzarKarim <karim.jazzar@gov.bc.ca>
2023-08-09 11:30:15 -0700

    Added address schema for Base Address to achieve proper validation in SB

--- src/components/base-address/BaseAddress.stories.ts
+++ src/components/base-address/BaseAddress.stories.ts
@@ -1,11 +1,39 @@
 import type { Meta } from '@storybook/vue'
 import { BaseAddress } from './index'
 import Vuetify from 'vuetify'
+import { required, maxLength } from 'vuelidate/lib/validators'
 
 const meta: Meta<typeof BaseAddress> = {
   title: 'component/BaseAddress'
 }
 export default meta
+
+const PersonAddressSchema = {
+  streetAddress: {
+    required,
+    maxLength: maxLength(50)
+  },
+  streetAddressAdditional: {
+    maxLength: maxLength(50)
+  },
+  addressCity: {
+    required,
+    maxLength: maxLength(40)
+  },
+  addressCountry: {
+    required
+  },
+  addressRegion: {
+    maxLength: maxLength(2)
+  },
+  postalCode: {
+    required,
+    maxLength: maxLength(15)
+  },
+  deliveryInstructions: {
+    maxLength: maxLength(80)
+  }
+}
 
 const Template = (args, { argTypes }) => ({
   vuetify: new Vuetify({ iconfont: 'mdi' }),
@@ -17,13 +45,13 @@ const Template = (args, { argTypes }) => ({
 export const DefaultBaseAddress = Template.bind({})
 DefaultBaseAddress['args'] = {
   editing: true,
-  schema: null,
+  schema: PersonAddressSchema,
   address: {}
 }
 export const FilledInBaseAddress = Template.bind({})
 FilledInBaseAddress['args'] = {
   editing: true,
-  schema: null,
+  schema: PersonAddressSchema,
   address: {
     streetAddress: '1234 Sesame Street',
     streetAddressAdditional: '4th Floor',
