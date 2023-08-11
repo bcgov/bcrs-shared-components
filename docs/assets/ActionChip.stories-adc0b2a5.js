bd51d41853274623efc2c01c96a7197cc39500f4
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2023-08-11 14:12:46 -0700

    16636 Added new component "SubMenu" (#195)
    * - added new component "SubMenu"
    
    * - rebuilt Storybook

---
+++ docs/assets/ActionChip.stories-adc0b2a5.js
@@ -0,0 +1,46 @@
+import{A as a,a as T}from"./ActionChip-73b76eea.js";import{V as h}from"./vuetify-abd37748.js";import"./vue.esm-b1e57a2f.js";import"./_plugin-vue2_normalizer-b11a0120.js";import"./_commonjsHelpers-042e6b4d.js";const k={title:"component/ActionChip"},s=(E,{argTypes:v})=>({vuetify:new h({iconfont:"mdi"}),props:Object.keys(v),components:{ActionChip:T},template:'<action-chip v-bind="$props" />'}),C={action:a.ADDED},w={action:a.REMOVED},A={action:a.EDITED},e=s.bind({});e.args={actionableItem:C};const o=s.bind({});o.args={actionableItem:w};const t=s.bind({});t.args={actionableItem:A,editedLabel:"CHANGED"};const r=s.bind({});r.args={actionableItem:A,editedLabel:"CORRECTED"};var i,c,p;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var n,m,d;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,b,l;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(l=(b=t.parameters)==null?void 0:b.docs)==null?void 0:l.source}}};var y,u,f;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(f=(u=r.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const I=["added","removed","editedAlteration","editedCorrection"];export{I as __namedExportsOrder,e as added,k as default,t as editedAlteration,r as editedCorrection,o as removed};
+//# sourceMappingURL=ActionChip.stories-adc0b2a5.js.map
