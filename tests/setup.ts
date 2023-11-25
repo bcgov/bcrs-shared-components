525313c2defc02b4c117a405df2902d7e5cf1a7d
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2023-11-24 16:11:11 -0800

    18526-code cleanup and fix configs for unit tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,11 +3,39 @@
  * It can save people time when writing tests, as they won't need to figure out
  * why some of the errors are showing up due to Vue not having the plugins it needs.
  */
-import Vue from 'vue'
-import Vue2Filters from 'vue2-filters'
-import Vuelidate from 'vuelidate'
-import Vuetify from 'vuetify'
+import { config } from '@vue/test-utils'
+import vuetify from '@/plugins/vuetify'
+import * as matchers from 'vitest-axe/matchers'
+import 'vitest-axe/extend-expect'
+import { expect } from 'vitest'
 
-Vue.use(Vue2Filters) // needed by SbcFeeSummary
-Vue.use(Vuelidate) // needed by BaseAddress
-Vue.use(Vuetify)
+// Extend vitest with axe matchers
+expect.extend(matchers)
+
+// Add properties to the wrapper
+config.global.plugins.push([vuetify])
+// Suppress Vue warnings
+config.global.config.warnHandler = () => null
+global.css = { supports: () => false }
+
+beforeAll(() => {
+  // Mock the ResizeObserver
+  const ResizeObserverMock = vi.fn(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn()
+  }))
+  // Stub the global ResizeObserver
+  vi.stubGlobal('ResizeObserver', ResizeObserverMock)
+
+  // Mock Sentry
+  vi.mock('@sentry/browser', () => ({
+    captureException: vi.fn()
+  }))
+
+  // Extend JSDOM with canvas to facilitate AA testing
+  Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+    value: vi.fn(),
+    writable: true
+  })
+})
