5a4da0f2518487e5e50c9798aed9c2d29299f6a6
tshyun24 <shaoyun.tong@gov.bc.ca>
2023-12-07 08:01:35 -0800

    update shareStructure.vue to vue3

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,6 +7,7 @@ import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VDataTable } from 'vuetify/components'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -17,6 +18,9 @@ export default createVuetify({
       mdi,
     },
   },
-  components,
+  components: {
+    ...components,
+    VDataTable
+  },
   directives,
 })
