0a1b2bb3851cfc74bda6cb5475e7e937ece5f121
ketaki-deodhar <ketaki.deodhar@gov.bc.ca>
2023-12-14 13:53:22 -0800

    18777-handle null in date fun and fix unit test

--- tests/unit/date-helper.spec.ts
+++ tests/unit/date-helper.spec.ts
@@ -2,10 +2,10 @@ import { shortPacificDate } from '../../src/utils/date-helper'
 
 describe('Date Helper', () => {
   it('returns short pacific date shortPacificDate()', () => {
-    expect(shortPacificDate('2021-07-01T00:00:00+00:00')).toBe('June 30, 2021')
+    expect(shortPacificDate(new Date('2021-06-30 07:00:00 GMT'))).toBe('June 30, 2021')
   })
 
   it('returns RangeError when empty string is passed shortPacificDate()', () => {
-    expect(() => shortPacificDate('')).toThrowError('Invalid time value')
+    expect(shortPacificDate('')).toBe(null)
   })
 })
