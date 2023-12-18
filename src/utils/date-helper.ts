bf180e065c3f842e9fb84c05a31c92ebf7bb6c29
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2023-12-18 09:01:19 -0800

    18974+18950 Minor InputFieldDatePicker Updates (#218)
    * Minor InputFieldDatePicker updates
    
    * Update version to 3.0.5
    
    ---------
    
    Co-authored-by: leodube-aot <leo.dube@aot-technologies.com>

--- src/utils/date-helper.ts
+++ src/utils/date-helper.ts
@@ -1,6 +1,8 @@
 import { isDate, isEmpty } from 'lodash'
 // Converts date to string and pacific date string
 // Example Output: August 11, 2023
+import { isDate } from 'lodash'
+
 export function shortPacificDate (date: Date | string): string {
   // safety check
   if (!isDate(date) || isNaN(date.getTime())) return null
@@ -9,4 +11,21 @@ export function shortPacificDate (date: Date | string): string {
     dateStyle: 'long',
     timeZone: 'America/Vancouver'
   }).format(new Date(date))
+}
+
+/**
+ * Converts a Date object to a date string (YYYY-MM-DD) in Pacific timezone.
+ * @example "2021-01-01 07:00:00 GMT" -> "2020-12-31"
+ * @example "2021-01-01 08:00:00 GMT" -> "2021-01-01"
+ */
+export function dateToYyyyMmDd (date: Date): string {
+  // safety check
+  if (!isDate(date) || isNaN(date.getTime())) return null
+
+  return date.toLocaleDateString('en-CA', {
+    timeZone: 'America/Vancouver',
+    month: 'numeric', // 12
+    day: 'numeric', // 31
+    year: 'numeric' // 2020
+  })
 }
