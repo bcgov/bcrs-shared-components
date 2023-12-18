d7240d42057d6dbf94267c05f0f6fee0179de91a
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2023-12-18 12:29:11 -0800

    - added correct name options to enum for AML (#223)
    Co-authored-by: Severin Beauvais <severin.beauvais@gov.bc.ca>

--- src/enums/correct-name-options.ts
+++ src/enums/correct-name-options.ts
@@ -1,4 +1,6 @@
 export enum CorrectNameOptions {
+  CORRECT_AML_ADOPT = 'correct-aml-adopt',
+  CORRECT_AML_NUMBERED = 'correct-aml-numbered',
   CORRECT_NAME = 'correct-name',
   CORRECT_NAME_TO_NUMBER = 'correct-name-to-number',
   CORRECT_NEW_NR = 'correct-new-nr'
