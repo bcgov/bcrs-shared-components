09b0854284fa1eed7882b0c0a6653898e5af1f26
James <james.paolo.p.garcia@gmail.com>
2024-01-10 12:21:48 -0800

    19003-vue3-upgrade-fee-summary

--- src/components/fee-summary/FeeSummary.vue
+++ src/components/fee-summary/FeeSummary.vue
@@ -12,7 +12,7 @@
       >
         <v-btn
           id="back-btn"
-          large
+          size="large"
           :loading="isLoading"
           @click="emitAction(FeeSummaryActions.BACK)"
         >
@@ -22,7 +22,7 @@
       <v-col class="pt-3">
         <v-btn
           id="cancel-btn"
-          large
+          size="large"
           :loading="isLoading"
           @click="emitAction(FeeSummaryActions.CANCEL)"
         >
@@ -32,7 +32,7 @@
       <v-col class="pt-3">
         <v-btn
           id="save-resume-later-btn"
-          large
+          size="large"
           :loading="isLoading"
           @click="emitAction(FeeSummaryActions.SAVE_RESUME_LATER)"
         >
@@ -42,7 +42,7 @@
       <v-col class="pt-3">
         <v-btn
           id="confirm-btn"
-          large
+          size="large"
           :disabled="hasConflicts"
           :loading="isLoading"
           @click="emitAction(FeeSummaryActions.CONFIRM)"
@@ -72,31 +72,31 @@ import SbcFeeSummary from './SbcFeeSummary.vue'
 export default class FeeSummary extends Vue {
   readonly FeeSummaryActions = FeeSummaryActions
 
-  /** Filing information to calculate fees. */
-  @Prop({ default: () => [] }) readonly filingData!: Array<FilingDataIF>
+/** Filing information to calculate fees. */
+@Prop({ default: () => [] }) readonly filingData!: Array<FilingDataIF>
 
-  /** URL for Sbc Fee Summary component to get fees. */
-  @Prop({ default: '' }) readonly payApiUrl!: string
+/** URL for Sbc Fee Summary component to get fees. */
+@Prop({ default: '' }) readonly payApiUrl!: string
 
-  /** Indicator that something isn't valid. */
-  @Prop({ default: false }) readonly hasConflicts!: boolean
+/** Indicator that something isn't valid. */
+@Prop({ default: false }) readonly hasConflicts!: boolean
 
-  /** Indicator that there is a request in progress. */
-  @Prop({ default: false }) readonly isLoading!: boolean
+/** Indicator that there is a request in progress. */
+@Prop({ default: false }) readonly isLoading!: boolean
 
-  /** Label for Confirm button. */
-  @Prop({ default: 'Confirm' }) readonly confirmLabel!: string
+/** Label for Confirm button. */
+@Prop({ default: 'Confirm' }) readonly confirmLabel!: string
 
-  /** Message to display if there is an error. */
-  @Prop({ default: '' }) readonly errorMessage!: string
+/** Message to display if there is an error. */
+@Prop({ default: '' }) readonly errorMessage!: string
 
-  /** Prop to indicate summary mode. */
-  @Prop({ default: false }) readonly isSummaryMode!: boolean
+/** Prop to indicate summary mode. */
+@Prop({ default: false }) readonly isSummaryMode!: boolean
 
-  /** Emit action event. */
-  @Emit('action')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  emitAction (action: FeeSummaryActions): void {}
+/** Emit action event. */
+@Emit('action')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+emitAction (action: FeeSummaryActions): void {}
 }
 </script>
 
@@ -104,34 +104,34 @@ export default class FeeSummary extends Vue {
 @import '@/assets/styles/theme.scss';
 
 #fee-summary {
-  .v-btn {
+.v-btn {
     width: 100%;
     border: 1.25px solid $app-blue;
     color: $app-blue;
     box-shadow: 0 1px 2px 0 rgba(33,37,41,0.2);
     border-radius: 4px;
-  }
+}
 
-  #confirm-btn {
+#confirm-btn {
     color: white;
     background-color: $app-blue;
     font-weight: bold;
-  }
+}
 
-  .v-btn[disabled] {
+.v-btn[disabled] {
     color: white !important;
     background-color: $app-blue !important;
     opacity: 0.2;
-  }
+}
 
-  .error-msg {
+.error-msg {
     font-size: 0.75rem;
     color: $app-red;
     text-align: center;
-  }
+}
 
-  :deep(.fee-list) {
+:deep(.fee-list) {
     padding-left: 0;
-  }
+}
 }
 </style>
