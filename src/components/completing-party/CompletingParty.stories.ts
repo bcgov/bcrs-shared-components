044926e079d8411b867a7a90272e8b77def475fd
Severin Beauvais <severin.beauvais@gov.bc.ca>
2024-01-11 13:16:13 -0800

    - same files as PR 232 (except the package lock files)

--- src/components/completing-party/CompletingParty.stories.ts
+++ src/components/completing-party/CompletingParty.stories.ts
@@ -1,43 +1,18 @@
-import type { Meta } from '@storybook/vue'
-import { CompletingParty } from './index'
-import { required, maxLength } from 'vuelidate/lib/validators'
+import { Meta } from '@storybook/vue3'
+import CompletingParty from './CompletingParty.vue'
+import { AddressIF } from '@bcrs-shared-components/interfaces'
 
-const meta: Meta<typeof CompletingParty> = {
-  title: 'component/CompletingParty'
-}
-export default meta
+export default {
+  title: 'Component/CompletingParty',
+  component: CompletingParty
+} as Meta
 
-const PersonAddressSchema = {
-  streetAddress: {
-    required,
-    maxLength: maxLength(50)
-  },
-  streetAddressAdditional: {
-    maxLength: maxLength(50)
-  },
-  addressCity: {
-    required,
-    maxLength: maxLength(40)
-  },
-  addressCountry: {
-    required
-  },
-  addressRegion: {
-    maxLength: maxLength(2)
-  },
-  postalCode: {
-    required,
-    maxLength: maxLength(15)
-  },
-  deliveryInstructions: {
-    maxLength: maxLength(80)
-  }
-}
-
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
+const Template = (args: AddressIF) => ({
   components: { CompletingParty },
-  template: '<v-card flat><completing-party v-bind="$props" /></v-card>'
+  setup () {
+    return { args }
+  },
+  template: '<completing-party v-bind="args" />'
 })
 
 export const Default = Template.bind({})
@@ -51,13 +26,10 @@ Default['args'] = {
       addressCountry: 'Canada',
       addressRegion: 'BC',
       deliveryInstructions: 'Test test test',
-      postalCode: 'V8V 1s8',
+      postalCode: 'V8V 1S8',
       streetAddress: '1234',
       streetAddressAdditional: 'Test Street Additional'
     }
   },
-  enableAddEdit: false,
-  addressSchema: PersonAddressSchema,
-  validate: false,
-  invalidSection: false
+  editing: true
 }
