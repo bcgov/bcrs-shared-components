d25c7f0211caba4cb782b549191001f57fc8989e
CameronEYDS <53542131+cameron-eyds@users.noreply.github.com>
2025-01-31 08:56:09 -0800

    25408 - DocumentId Component: Include BCRS Shared Rebuild (#265)
    * Bcrs Rebuild: Includes DocumentId Component
    
    * rebuild
    
    * Misc unit test fixes
    
    * Restore .nojekyll
    
    * Dependency Update and Rebuild
    
    * Styling updates, added error story and rebuild

---
+++ docs/assets/ActionChip.stories-bbaf1762.js
@@ -0,0 +1,46 @@
+import{A as a,a as T}from"./ActionChip-308408f3.js";import{V as h}from"./vuetify-b9fb25b0.js";import"./vue.esm-b1e57a2f.js";import"./Prop-817ec8de.js";import"./_plugin-vue2_normalizer-2bbd088e.js";import"./_commonjsHelpers-de833af9.js";const I={title:"component/ActionChip"},s=(E,{argTypes:v})=>({vuetify:new h({iconfont:"mdi"}),props:Object.keys(v),components:{ActionChip:T},template:'<action-chip v-bind="$props" />'}),C={action:a.ADDED},w={action:a.REMOVED},A={action:a.EDITED},e=s.bind({});e.args={actionableItem:C};const o=s.bind({});o.args={actionableItem:w};const t=s.bind({});t.args={actionableItem:A,editedLabel:"CHANGED"};const r=s.bind({});r.args={actionableItem:A,editedLabel:"CORRECTED"};var i,p,c;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var n,m,d;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(d=(m=o.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var g,b,l;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(l=(b=t.parameters)==null?void 0:b.docs)==null?void 0:l.source}}};var y,u,f;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  vuetify: new Vuetify({
+    iconfont: 'mdi'
+  }),
+  props: Object.keys(argTypes),
+  components: {
+    ActionChip
+  },
+  template: '<action-chip v-bind="$props" />' // $props comes from args below
+})`,...(f=(u=r.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const S=["added","removed","editedAlteration","editedCorrection"];export{S as __namedExportsOrder,e as added,I as default,t as editedAlteration,r as editedCorrection,o as removed};
+//# sourceMappingURL=ActionChip.stories-bbaf1762.js.map
---
+++ docs/assets/FolioNumberInput.stories-00c48cf6.js
@@ -0,0 +1,10 @@
+import{F as s}from"./FolioNumberInput-8f91eff3.js";import"./vue.esm-b1e57a2f.js";import"./Prop-817ec8de.js";import"./Watch-c62128e4.js";import"./_plugin-vue2_normalizer-2bbd088e.js";const b={title:"component/FolioNumberInput"},m=(a,{argTypes:p})=>({props:Object.keys(p),components:{FolioNumberInput:s},template:'<folio-number-input v-bind="$props" />'}),o=m.bind({});o.args={folioNumber:"01234567890123456789012345678901234567890123456789ABC@123/*!$ ..."};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    FolioNumberInput
+  },
+  template: '<folio-number-input v-bind="$props" />' // $props comes from args below
+})`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const d=["Default"];export{o as Default,d as __namedExportsOrder,b as default};
+//# sourceMappingURL=FolioNumberInput.stories-00c48cf6.js.map
