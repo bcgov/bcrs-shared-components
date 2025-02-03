09e73e0957ba0757074bce00cf353564ec445e6a
Thayne Werdal <thayne.werdal@gov.bc.ca>
2025-02-03 08:46:34 -0800

    24102-Updating entity-status enums to match suggested structure
    Signed-off-by: Thayne Werdal <thayne.werdal@gov.bc.ca>

--- src/enums/entity-states.ts
+++ src/enums/entity-states.ts
@@ -1,17 +1,27 @@
 export enum EntityStates {
   ACTIVE = 'ACTIVE',
-  HISTORICAL = 'HISTORICAL',
-  LIQUIDATION = 'LIQUIDATION'
-}
-
-export enum DraftStatus {
-  ACTIVE = 'ACTIVE',
   APPROVED = 'APPROVED',
   AWAITING_REVIEW = 'AWAITING_REVIEW',
   CHANGE_REQUESTED = 'CHANGE_REQUESTED',
   DRAFT = 'DRAFT',
+  HISTORICAL = 'HISTORICAL',
+  LIQUIDATION = 'LIQUIDATION',
   REJECTED = 'REJECTED',
   PENDING = 'PENDING',
   PAID = 'PAID',
   WITHDRAWN = 'WITHDRAWN'
+}
+
+export enum EntityStateStatus {
+  ACTIVE = 'Active',
+  APPROVED = 'Approved',
+  AWAITING_REVIEW = 'Pending Staff Review',
+  CHANGE_REQUESTED = 'Change Requested',
+  DRAFT = 'Draft',
+  HISTORICAL = 'Historical',
+  LIQUIDATION = 'Liquidation',
+  REJECTED = 'Rejected',
+  PENDING = 'Pending',
+  PAID = 'Paid',
+  WITHDRAWN = 'Withdrawn'
 }
