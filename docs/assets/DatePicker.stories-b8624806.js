62d34afaf6a7be62ea73592b4546f95bece3423a
Severin Beauvais <severin.beauvais@gov.bc.ca>
2025-03-07 12:27:49 -0800

    - updated Storybook

---
+++ docs/assets/DatePicker.stories-b8624806.js
@@ -0,0 +1,58 @@
+import{D as j}from"./DatePicker-7efa3707.js";import"./_plugin-vue2_normalizer-b11a0120.js";import"./vue.esm-b1e57a2f.js";import"./Watch-62f8b699.js";import"./validation-mixin-aefd6a63.js";import"./lodash-f6b38868.js";import"./_commonjsHelpers-de833af9.js";const A={title:"component/DatePicker"},n="2021-03-01",L="2021-06-01",e=(c,{argTypes:S})=>({props:Object.keys(S),components:{DatePicker:j},template:'<date-picker v-bind="$props" />'}),r=e.bind({});r.args={title:"Date",errorMsg:"",inputRules:[],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0};const a=e.bind({});a.args={title:"Date",errorMsg:"You have some validation errors",inputRules:[],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0};const t=e.bind({});t.args={title:"Date with validation",errorMsg:"",inputRules:[c=>!!c||"A date is required."],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0};const s=e.bind({});s.args={title:"Date with rules",errorMsg:"",inputRules:[],disablePicker:!1,showCurrent:n,minDate:n,maxDate:L,nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0};const o=e.bind({});o.args={title:"Date with initial value",initialValue:"2021-11-18",errorMsg:"",inputRules:[],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0};const i=e.bind({});i.args={title:"Date",errorMsg:"",hint:"this is a hint.",inputRules:[],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0,persistentHint:!0};const p=e.bind({});p.args={title:"Date clearable",errorMsg:"",hint:"",inputRules:[],disablePicker:!1,minDate:"",maxDate:"",nudgeTop:0,nudgeBottom:0,nudgeRight:0,nudgeLeft:0,persistentHint:!0,clearable:!0};var d,m,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(g=(m=r.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var l,u,k;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(k=(u=a.parameters)==null?void 0:u.docs)==null?void 0:k.source}}};var b,D,P;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(P=(D=t.parameters)==null?void 0:D.docs)==null?void 0:P.source}}};var f,y,T;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(T=(y=s.parameters)==null?void 0:y.docs)==null?void 0:T.source}}};var h,$,R;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(R=($=o.parameters)==null?void 0:$.docs)==null?void 0:R.source}}};var v,x,w;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(w=(x=i.parameters)==null?void 0:x.docs)==null?void 0:w.source}}};var M,B,O;p.parameters={...p.parameters,docs:{...(M=p.parameters)==null?void 0:M.docs,source:{originalSource:`(args, {
+  argTypes
+}) => ({
+  props: Object.keys(argTypes),
+  components: {
+    DatePicker
+  },
+  template: '<date-picker v-bind="$props" />' // $props comes from args below
+})`,...(O=(B=p.parameters)==null?void 0:B.docs)==null?void 0:O.source}}};const Y=["datePickerBaseline","datePickerError","datePickerValidation","datePickerMinMax","datePickerInitialValue","datePickerHint","datePickerClearable"];export{Y as __namedExportsOrder,r as datePickerBaseline,p as datePickerClearable,a as datePickerError,i as datePickerHint,o as datePickerInitialValue,s as datePickerMinMax,t as datePickerValidation,A as default};
+//# sourceMappingURL=DatePicker.stories-b8624806.js.map
