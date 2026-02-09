412e0bc9187ad33c3f4956434c6fa633523c8d85
Séverin Beauvais <severinbeauvais@users.noreply.github.com>
2026-02-09 13:00:37 -0800

    31953 Added new validators package (#301)
    * - added new package declaration
    - created validators folder
    - added index file
    - added initial package file
    - added ValidatePostalCode file
    
    * - empty postal code is valid
    - added unit tests
    
    ---------
    
    Co-authored-by: Severin Beauvais <severin.beauvais@gov.bc.ca>

---
+++ src/validators/validate-postal-code.ts
@@ -0,0 +1,14 @@
+/**
+ * Canadian postal code regex (eg, accepts A1A 1A1 or A1A1A1).
+ * Ref: https://en.wikipedia.org/wiki/Postal_codes_in_Canada
+ */
+const CanadaPostalCodeRegex = /^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-Z][0-9]$/i
+
+/** Custom validator for postal codes. */
+export function validatePostalCode (value: string, parentVm: any): boolean {
+  // if Canada, validate postal code format
+  // empty value is considered valid -- "required" validation is handled separately
+  if (parentVm.addressCountry === 'CA') return !value || CanadaPostalCodeRegex.test(value)
+  // otherwise, no validation
+  return true
+}
